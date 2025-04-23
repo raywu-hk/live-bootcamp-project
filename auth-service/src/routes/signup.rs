fcb9486e78161629e606221952f2d5aaa62aeb27
RayW <raywu1988@me.com>
2025-04-23 16:25:58 +0100

    [Sprint 1][Task 7] test malform payload for signup

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,5 +1,15 @@
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-pub async fn signup() -> impl IntoResponse {
+use axum::Json;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct SignupRequest {
+    pub email: String,
+    pub password: String,
+    #[serde(rename = "requires2FA")]
+    pub requires_2fa: bool,
+}
+pub async fn signup(Json(request): Json<SignupRequest>) -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,11 +1,21 @@
 use crate::helpers::TestApp;
-use axum::http::StatusCode;
 
 #[tokio::test]
-async fn signup_return_ok() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
+    let random_email = TestApp::get_random_email();
+    let test_cases = [serde_json::json!({
+    "password": "test",
+    "requires2FA": true
+    })];
 
-    let response = app.post_signup().await;
-
-    assert_eq!(response.status().as_u16(), StatusCode::OK);
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "failed for input {:?}",
+            test_case
+        );
+    }
 }
