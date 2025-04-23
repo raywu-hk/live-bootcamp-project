bd97e9f8ed44e2cc34df57047a8f26656a0dc7c8
RayW <raywu1988@me.com>
2025-04-23 11:31:08 +0100

    [Sprint 1][Task 4] restructure auth-service

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,30 @@
+use axum::serve::Serve;
+use axum::Router;
+use std::error::Error;
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = axum::Router::new().nest_service("/", ServeDir::new("assets"));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        let app = Application { server, address };
+        Ok(app)
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
