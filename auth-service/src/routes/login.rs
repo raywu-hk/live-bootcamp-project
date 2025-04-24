9ab7aafdfc9143b952c5c1c289f7a2a77f2e422e
RayW <raywu1988@me.com>
2025-04-24 22:55:49 +0100

    [Sprint 3][Task 1] Implement log-in route error cases

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,33 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use crate::domain::{AuthAPIError, Email, Password};
+use crate::AppState;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::{response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+pub async fn login(
+    state: State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = Email::parse(&request.email).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password =
+        Password::parse(&request.password).map_err(|_| AuthAPIError::InvalidCredentials)?;
+
+    let user_store = state.user_store.read().await;
+
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return Err(AuthAPIError::IncorrectCredentials);
+    }
+
+    let _user = user_store
+        .get_user(&email)
+        .await
+        .map_err(|_| AuthAPIError::UnexpectedError);
+
+    Ok(StatusCode::OK.into_response())
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,11 +1,60 @@
 use crate::helpers::TestApp;
-use axum::http::StatusCode;
+use reqwest::StatusCode;
+use serde_json::json;
 
 #[tokio::test]
-async fn login_return_ok() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
+    let payload = json!({
+          "password": "password",
+    });
 
-    let response = app.post_login().await;
+    let response = app.post_login(&payload).await;
 
-    assert_eq!(response.status(), StatusCode::OK);
+    assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+    let signup_payload = json!({
+        "email": "user@example.com",
+        "password": "password",
+        "requires2FA":false
+    });
+    let signup_result = app.post_signup(&signup_payload).await;
+
+    assert_eq!(signup_result.status(), StatusCode::CREATED);
+
+    // Call the log-in route with invalid credentials and assert that a
+    // 400 HTTP status code is returned along with the appropriate error message.
+    let login_payload = json!({
+        "email": "@example.com",
+        "password": "password",
+    });
+    let login_result = app.post_login(&login_payload).await;
+
+    assert_eq!(login_result.status(), StatusCode::BAD_REQUEST);
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+    let signup_payload = json!({
+        "email": "user@example.com",
+        "password": "password",
+        "requires2FA":false
+    });
+    let signup_result = app.post_signup(&signup_payload).await;
+
+    assert_eq!(signup_result.status(), StatusCode::CREATED);
+    // Call the log-in route with incorrect credentials and assert
+    // that a 401 HTTP status code is returned along with the appropriate error message.
+    let login_payload = json!({
+        "email": "user@example.com",
+        "password": "wrong password",
+    });
+    let login_result = app.post_login(&login_payload).await;
+
+    assert_eq!(login_result.status(), StatusCode::UNAUTHORIZED);
 }
